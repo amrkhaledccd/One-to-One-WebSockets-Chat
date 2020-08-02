d0947f56e5e400081e4d22c1c4eb12763887363c
swapyinc <swapyinc@gmail.com>
2020-08-02 16:53:04 +0200

    remove topic and queue destination prefixes

--- chat-service/src/main/java/com/amr/chatservice/configuration/WebSocketConfig.java
+++ chat-service/src/main/java/com/amr/chatservice/configuration/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker( "/topic", "/queue", "/user");
+        config.enableSimpleBroker( "/user");
         config.setApplicationDestinationPrefixes("/app");
         config.setUserDestinationPrefix("/user");
     }
